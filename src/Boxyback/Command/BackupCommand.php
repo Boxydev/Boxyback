e19be02283b998d3a70b93f2d978ef7665e676d4
Matthieu Mota <matthieumota@gmail.com>
2016-05-21 17:09:00 +0200

    New backupCommand

---
+++ src/Boxyback/Command/BackupCommand.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Boxyback\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Yaml\Parser;
+
+class BackupCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('backup')
+            ->setDescription('Backup an or many Apps')
+            ->addArgument(
+                'yamlArgument',
+                InputArgument::REQUIRED,
+                "What's App do you want to backup (YML File)?"
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $date = '$(date +%d-%m-%Y-%H-%M-%S)';
+
+        if ($yamlArgument = $input->getArgument('yamlArgument')) {
+            $yaml = new Parser();
+            $apps = $yaml->parse(file_get_contents($yamlArgument))['apps'];
+        }
+        
+        foreach($apps as $app){
+        	$app = (object) $app;
+
+            system('mkdir -p /home/matthieu/backups/'.$app->id);
+
+            if(property_exists($app, 'database') && property_exists($app, 'password')){
+                system('mysqldump --protocol=socket -S /run/mysql-default/mysqld.sock --user='.$app->user.' --password='.$app->password.' '.$app->database.' | gzip > /home/matthieu/backups/'.$app->id.'/dump_'.$date.'.sql.gz');
+            }
+
+            if($app->type=="all"){
+            	system('tar -zcvf /home/matthieu/backups/'.$app->id.'/archive_'.$date.'.tar.gz -C /home/'.$app->user.'/'.$app->folder.' .');
+			}
+
+            system('find /home/matthieu/backups/'.$app->id.'/ -type f -mtime +'.($app->save-1).' -exec rm -v {} \;');
+
+        }
+    }
+}
