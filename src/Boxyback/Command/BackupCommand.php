0f041604fdaa7e87aef9c659b6e48852f75b6163
Matthieu Mota <matthieumota@gmail.com>
2016-06-13 08:53:25 +0200

    Fix #4, wait enable rotate

--- src/Boxyback/Command/BackupCommand.php
+++ src/Boxyback/Command/BackupCommand.php
@@ -25,12 +25,16 @@ class BackupCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $date = '$(date +%d-%m-%Y-%H-%M-%S)';
+        $date = '$(date +%Y-%m-%d-%H-%M-%S)';
 
         if ($yamlArgument = $input->getArgument('yamlArgument')) {
             $yaml = new Parser();
             $apps = $yaml->parse(file_get_contents($yamlArgument))['apps'];
         }
+
+        $script = file_get_contents(__DIR__."/rotate.php");
+        file_put_contents('rotate', $script);
+        chmod('rotate', 0755);
         
         foreach($apps as $app){
         	$app = (object) $app;
@@ -45,8 +49,12 @@ class BackupCommand extends Command
             	system('tar -zcvf /home/matthieu/backups/'.$app->id.'/archive_'.$date.'.tar.gz -C /home/'.$app->user.'/'.$app->folder.' .');
 			}
 
-            system('find /home/matthieu/backups/'.$app->id.'/ -type f -mtime +'.($app->save-1).' -exec rm -v {} \;');
+            //system('find /home/matthieu/backups/'.$app->id.'/ -type f -mtime +'.($app->save-1).' -exec rm -v {} \;');
+
+            system('./rotate /home/matthieu/backups/'.$app->id.'/');
 
         }
+
+        unlink('rotate');
     }
 }
